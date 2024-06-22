96f5f572055e840f010e4171a5bb7cca637ed05b
KVinqT <kevinqt4you@gmail.com>
2024-06-23 02:12:44 +0630

    reverse string with recursive

--- Exercises/Reverse-String/recursive/ReverseString.java
+++ Exercises/Reverse-String/recursive/ReverseString.java
@@ -1,5 +1,18 @@
-package Exercises.Reverse-String.recursive;
-
+import java.util.Scanner;
 public class ReverseString {
     
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String str= sc.nextLine();
+        System.out.println("Reverse string is "+ reverseString(str));
+    }
+    public static String reverseString(String str){
+        if(str.length() <= 1){
+            return str;
+        }
+        String rightSideArray = reverseString(str.substring(str.length() / 2));
+        String leftSideArray = reverseString(str.substring(0,str.length() / 2));
+        return rightSideArray + leftSideArray;
+    }
 }
